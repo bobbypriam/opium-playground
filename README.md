b22ee1aec42bbcece96dc909bdb2a1af63082dc6
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-07-11 22:23:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Opium Playground
+
+My playground of exploring [Opium](https://github.com/rgrinberg/opium), a (Sinatra|Express|Flask|SlimPHP)-like web toolkit for OCaml.
+
+## Getting started
+
+Tested in OCaml 4.04.2.
+
+```bash
+$ opam install ocp-indent merlin jbuilder opium
+$ jbuilder build hello_world.exe
+$ _build/default/hello_world.exe
+```
---
+++ hello_world.ml
@@ -0,0 +1,31 @@
+open Opium.Std
+
+type person = {
+  name: string;
+  age: int;
+}
+
+let json_of_person { name ; age } =
+  let open Ezjsonm in
+  dict [ "name", (string name)
+       ; "age", (int age) ]
+
+let hello_handler req =
+  `String ("Hello " ^ param req "name") |> respond'
+
+let hello_route = get "/hello/:name" hello_handler
+
+let person_handler req =
+  let person = {
+      name = param req "name";
+      age = "age" |> param req |> int_of_string
+    } in
+    `Json (person |> json_of_person) |> respond'
+
+let person_route = get "/person/:name/:age" person_handler
+
+let _ =
+  App.empty
+  |> hello_route
+  |> person_route
+  |> App.run_command
